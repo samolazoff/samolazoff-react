f8eaaf08bd1938c614899d5cb2c509b34f3617b1
Samolazoff <samolazov.roman.alex@gmail.com>
2025-10-14 06:44:34 +0300

    Create Main presen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import './Home.scss';
 
 import About from '../../layouts/About/About';
 import Present from '../../layouts/Present/Present';
+import MainPresent from '../../components/MainPresent/MainPresent';
 
 const Home = () => {
     return (
         <>
             <About/>
             <Present/>
+            <MainPresent/>
         </>
     );
 };
