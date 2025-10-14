6567899064f86bf58c3889181b5ef8a9883e60af
Samolazoff <samolazov.roman.alex@gmail.com>
2025-10-14 18:56:02 +0300

    Add blog-mini

--- src/components/BlogMini/BlogMini.jsx
+++ src/components/BlogMini/BlogMini.jsx
@@ -1,6 +1,5 @@
 import './BlogMini.scss';
 
-import img from '../../static/img/mine/site.png';
 
 import { Link } from 'react-router-dom';
 
@@ -11,12 +10,16 @@ const BlogMini = () => {
     return (
         <section className='blog-mini'>
             <TitleBlock txt="What's the news"/>
+            <div className="blog-mini__title-txt">
+                <span>I'm constantly evolving and</span>
+                <span>I have something to share with you.</span>
+            </div>
             <ul className="blog-mini-items">
                 <li className="blog-mini-item">
                     <Link to = "/" className='blog-mini-card'>
                         <data value="14-10-2025">14.10.2025</data>
                         <h3 className="blog-mini-card__title">Portfolio</h3>
-                        <span className="blog-mini-card__sibtitle">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quidem nobis nesciunt, repellat, numquam sapiente, </span>
+                        <span className="blog-mini-card__sibtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facilis a quas assumenda fuga vero velit possimus? Blanditiis quaerat maxime nam laborum tempore accusantium laboriosam, soluta id, iste aliquam corrupti? </span>
                     </Link>
                 </li>
                     <li className="blog-mini-item">
@@ -27,6 +30,7 @@ const BlogMini = () => {
                     </Link>
                 </li>
             </ul>
+            <Btm txt="Read More"/>
         </section>
     );
 };
