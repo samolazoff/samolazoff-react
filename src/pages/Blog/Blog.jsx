67a365bd9086a33ff8f6f1c3af8a11a223e2762b
Samolazoff <samolazov.roman.alex@gmail.com>
2025-10-29 12:45:07 +0300

    Create Blog

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -8,10 +8,27 @@ const BLog = () => {
     return (
         <section className='blog'>
             <TitleBlock txt='My Blog'/>
-            <div className="blog__title-txt" id='contacts'>
-                <span>This is</span>
-                <span>My way.</span>
-            </div>
+            <ul className="blog-items">
+                <li className="blog-item">
+                    <div className="blog-item-box">
+                        <h4 className="blog-item__title">The first news</h4>
+                        <data value="29-10-2025">29/10/2025</data>
+                    </div>
+                    <div className="blog-item-article">
+                        <p className="blog-item-article__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore perferendis inventore distinctio aut, atque natus ullam modi voluptatibus libero iusto saepe possimus officiis. Error sequi totam quam ratione odio quas unde dolorum numquam temporibus quasi, delectus doloribus. Molestias vitae nulla, exercitationem autem consequatur saepe tenetur, recusandae porro quo odio voluptas assumenda sequi. Atque, neque a? Ducimus voluptates nesciunt sapiente dolorem non, autem obcaecati doloremque.</p>
+                    </div>
+                </li>
+                <li className="blog-item">
+                    <div className="blog-item-box">
+                        <h4 className="blog-item__title">The first news</h4>
+                        <data value="29-10-2025">29/10/2025</data>
+                    </div>
+                    <div className="blog-item-article">
+                        <p className="blog-item-article__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore perferendis inventore distinctio aut, atque natus ullam modi voluptatibus libero iusto saepe possimus officiis. Error sequi totam quam ratione odio quas unde dolorum numquam temporibus quasi, delectus doloribus. Molestias vitae nulla, exercitationem autem consequatur saepe tenetur, recusandae porro quo odio voluptas assumenda sequi. Atque, neque a? Ducimus voluptates nesciunt sapiente dolorem non, autem obcaecati doloremque.</p>
+                        <p className="blog-item-article__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore perferendis inventore distinctio aut, atque natus ullam modi voluptatibus libero iusto saepe possimus officiis. Error sequi totam quam ratione odio quas unde dolorum numquam temporibus quasi, delectus doloribus. Molestias vitae nulla, exercitationem autem consequatur saepe tenetur, recusandae porro quo odio voluptas assumenda sequi. Atque, neque a? Ducimus voluptates nesciunt sapiente dolorem non, autem obcaecati doloremque.</p>
+                    </div>
+                </li>
+            </ul>
         </section>
     );
 };
